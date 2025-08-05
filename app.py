95c57631ea28ee5bef868ed2acfbf8198416e47a
subhadip07 <subhadipbag18@outlook.com>
2025-08-05 21:31:13 +0530

    added multiply function

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ def subtract(a, b):
     return a - b
 
 
+def multiply(a, b):
+    return a * b
+
+
 result1 = add(4, 5)
 result2 = subtract(9, 3)
+result3 = multiply(3, 5)
 
-print(result1, result2)
+print(result1, result2, result3)
